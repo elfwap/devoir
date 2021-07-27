57a89cca450c3bb2e19a0e1f83bd5d3402eb3a4c
Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
2021-07-27 10:48:04 +0100

    Create Devoir.php
    Building block to devoir framework

---
+++ src/Devoir.php
@@ -0,0 +1,33 @@
+<?php
+namespace Devoir;
+
+/**
+ *
+ * @author Muhammad Tahir Abdullahi
+ * @copyright Copyright (c) Elftech Inc.
+ * @license https://opensource.org/licenses/mit-license.php MIT License
+ *        
+ */
+
+class Devoir
+{
+    // TODO - Insert your code here
+
+    /**
+     */
+    public function __construct()
+    {
+
+        // TODO - Insert your code here
+    }
+
+    /**
+     */
+    function __destruct()
+    {
+
+        // TODO - Insert your code here
+    }
+    
+}
+
