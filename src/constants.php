736f71f55b47af35221c7078eefbae6409506b6e
Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
2021-07-27 10:46:40 +0100

    Created constants.php
    Contains constant global variables

---
+++ src/constants.php
@@ -0,0 +1,21 @@
+<?php
+namespace Devoir;
+
+if (!defined('YES')) {
+    define('YES', true);
+}
+if (!defined('Yes')) {
+    define('Yes', true);
+}
+if (!defined('yes')) {
+    define('yes', true);
+}
+if (!defined('NO')) {
+    define('NO', false);
+}
+if (!defined('No')) {
+    define('No', false);
+}
+if (!defined('no')) {
+    define('no', false);
+}
