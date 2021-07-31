b16cf57cc13e1712f4920400faa99ed040ec5106
Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
2021-07-31 21:26:04 +0100

    Update constants.php

--- src/constants.php
+++ src/constants.php
@@ -34,3 +34,17 @@ if(!defined('SEC_MONTH'))
     define('SEC_MONTH', 2592000);
 if(!defined('SEC_YEAR'))
     define('SEC_YEAR', 31536000);
+if(!defined('MIL_SECOND'))
+	define('MIL_SECOND', 1000);
+if(!defined('MIL_MINUTE'))
+	define('MIL_MINUTE', 60000);
+if(!defined('MIL_HOUR'))
+	define('MIL_HOUR', 3600000);
+if(!defined('MIL_DAY'))
+	define('MIL_DAY', 86400000);
+if(!defined('MIL_WEEK'))
+	define('MIL_WEEK', 604800000);
+if(!defined('MIL_MONTH'))
+	define('MIL_MONTH', 2592000000);
+if(!defined('MIL_YEAR'))
+	define('MIL_YEAR', 31536000000);
