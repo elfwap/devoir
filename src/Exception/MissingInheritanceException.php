82022e6bff5e9d6e895f82f6c6cc7037df34afd3
Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
2021-08-26 09:01:10 +0100

    Create Missing Inheritance Exception
    Thrown when there's failure to extent Controller, Model, View and other
    major components

---
+++ src/Exception/MissingInheritanceException.php
@@ -0,0 +1,46 @@
+<?php
+namespace Devoir\Exception;
+
+use \Throwable;
+use \ReflectionClass;
+
+/**
+ *
+ * @namespace Devoir\Exception
+ * @author Muhammad Tahir Abdullahi
+ * @copyright Copyright (c) Elftech Inc.
+ * @package elfwap/devoir
+ * @license https://opensource.org/licenses/mit-license.php MIT License
+ *
+ */
+class MissingInheritanceException extends DevoirException
+{
+	/**
+	 *
+	 * @param mixed $message
+	 * @param int $code
+	 * @param Throwable $previous
+	 */
+	public function __construct($message = null, int $code = null, Throwable $previous = null)
+	{
+		if(is_array($message) && count($message) <> 2){
+			throw new DevoirException("Array argument must contain two (2) items. `" . count($message) . "` given.");
+		}
+		$this->template = "Class `%s` must extend class `%s`.";
+		$this->code = MISSING_INHERITANCE_EXCEPTION_CODE;
+		parent::__construct($message, $code, $previous);
+	}
+
+	/**
+	 */
+	function __destruct()
+	{
+	}
+	/**
+	 * @return \Devoir\Exception\MissingInheritanceException
+	 */
+	public static function newInstance($message, ?int $code = null, ?Throwable $previous = null) {
+		return (new ReflectionClass(self::class))->newInstanceArgs([$message, $code, $previous]);
+	}
+}
+
