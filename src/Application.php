34d614390f25b7494040bef139174946bb9931cf
Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
2021-10-21 15:05:49 +0100

    Update Application.php

--- src/Application.php
+++ src/Application.php
@@ -18,10 +18,10 @@ class Application
 		if(is_dir($systemDir)){
 			$constants = $systemDir . DIRECTORY_SEPARATOR . 'constants.php';
 			$functions = $systemDir . DIRECTORY_SEPARATOR . 'functions.php';
-			if(is_file($constants) && file_exists($constants)){
+			if(file_exists($constants)){
 				require_once $constants;
 			}
-			if(is_file($functions) && file_exists($functions)){
+			if(file_exists($functions)){
 				require_once $functions;
 			}
 		}
@@ -29,11 +29,16 @@ class Application
 		if(is_dir($devoirSystemDir)){
 			$devoirConstants = $devoirSystemDir . DIRECTORY_SEPARATOR . 'constants.php';
 			$devoirFunctions = $devoirSystemDir . DIRECTORY_SEPARATOR . 'functions.php';
-			if(is_file($devoirConstants) && file_exists($devoirConstants)) require_once $devoirConstants;
-			if(is_file($devoirFunctions) && file_exists($devoirFunctions)) require_once $devoirFunctions;
+			if(file_exists($devoirConstants)) require_once $devoirConstants;
+			if(file_exists($devoirFunctions)) require_once $devoirFunctions;
 		}
-		$controller = new Controller(null, null, [], $systemDir);
+		$router = new Router($systemDir);
+		if ($router->match() == YES) {
+			$controller = new Controller($router->getController(), $router->getAction(), (array) $router->getParams(), $systemDir);
+		}
+		else {
+			$controller = new Controller(null, null, [], $systemDir);
+		}
 		$controller->run();
 	}
 }
-
