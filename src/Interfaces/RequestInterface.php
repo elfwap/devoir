5a8ab42bf30d94d9cbcdaddfcd6c3c98857a2bd8
Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
2021-10-21 15:08:26 +0100

    Update RequestInterface.php

--- src/Interfaces/RequestInterface.php
+++ src/Interfaces/RequestInterface.php
@@ -12,14 +12,88 @@ namespace Devoir\Interfaces;
  */
 interface RequestInterface
 {
+	/**
+	 * Checks whether the request is `GET`.
+	 * @param Devoir\Interfaces\RequestInterface $request
+	 * @return bool true|false
+	 */
 	public function isGet(RequestInterface $request): bool;
+	/**
+	 * Checks whether the request is `POST`.
+	 * @param Devoir\Interfaces\RequestInterface $request
+	 * @return bool true|false
+	 */
 	public function  isPost(RequestInterface $request): bool;
+	/**
+	 * Checks whether the request is `PATCH`.
+	 * @param Devoir\Interfaces\RequestInterface $request
+	 * @return bool true|false
+	 */
 	public function isPatch(RequestInterface $request): bool;
+	/**
+	 * Checks whether the request is `DELETE`.
+	 * @param Devoir\Interfaces\RequestInterface $request
+	 * @return bool true|false
+	 */
 	public function isDelete(RequestInterface $request): bool;
+	/**
+	 * Checks whether the request is `PUT`.
+	 * @param Devoir\Interfaces\RequestInterface $request
+	 * @return bool true|false
+	 */
 	public function isPut(RequestInterface $request): bool;
+	/**
+	 * Checks whether the request is `JSON`.
+	 * @param Devoir\Interfaces\RequestInterface $request
+	 * @return bool true|false
+	 */
 	public function isJSON(RequestInterface $request): bool;
+	/**
+	 * Checks whether the request is `GET|PUT|POST` and so on.
+	 * @param string $type string value of type to check on.
+	 * @param Devoir\Interfaces\RequestInterface $request
+	 * @return bool true|false
+	 */
 	public function is(?string $type, RequestInterface $request):bool;
-	public function setData($data);
-	public function getData($data = null);
-	
+	/**
+	 * Set the query data to post request before dispatch.
+	 * @param string $data
+	 * @param mixed $value
+	 */
+	public function setData(?string $data, mixed $value);
+	/**
+	 * Get a single query data from post request
+	 * @param string|null $data
+	 */
+	public function getData(?string $data = null);
+	/**
+	 * Get the host name of the requested address.
+	 * @return string Host name as string value.
+	 */
+	public function getHost(): string;
+	/**
+	 * Get the port number of the requested address.
+	 * @return int Port number as integer value.
+	 */
+	public function getPort(): int;
+	/**
+	 * Get URL Scheme for the current request.
+	 * @return string
+	 */
+	public function getScheme(): string;
+	/**
+	 * 
+	 * @return string
+	 */
+	public function getPath(): string;
+	/**
+	 * @param string|null $data
+	 * @return string|array
+	 */
+	public function getQuery(?string $data = null);
+	/**
+	 * @param string|null $index
+	 * @return mixed|array
+	 */
+	public function getServer(?string $index = null);
 }
