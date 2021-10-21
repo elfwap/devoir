6ae89359e970e2997350e9e4148629f74180e033
Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
2021-10-21 15:09:07 +0100

    Update functions.php

--- system/functions.php
+++ system/functions.php
@@ -4,18 +4,24 @@ use Devoir\Devoir;
 use Devoir\Controller;
 
 if(!function_exists("isController")){
-    function isController($component) {
+	/**
+	 * Checks whether the value of argument passed `is a` Devoir\Controller. 
+	 */
+    function isController($component): bool
+	{
         return is_a($component, Controller::class, true);
        
     }
 }
 /* if(!function_exists("isModel")){
-	function isModel($component) {
+	function isModel($component): bool
+	{
 		return is_a($component, Model::class, true);
 	}
 }
 if(!function_exists("isView")){
-	function isView($component) {
+	function isView($component): bool
+	{
 		return is_a($component, View::class, true);
 	}
 } */
@@ -23,4 +29,12 @@ if(!function_exists("isDevoir")){
 	function isDevoir($component) {
 		return is_a($component, Devoir::class, true);
 	}
+}
+/**
+ * Checks whether the passed argument value is null
+ * @param mixed $value the value to passed as argument.
+ * @return bool true|false
+ */
+function isNull(mixed $value): bool{
+	return ($value === null);
 }
