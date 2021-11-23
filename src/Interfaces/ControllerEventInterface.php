8f2bd6b2536edcfb52561057f66f7ec0b91f741c
Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
2021-11-23 06:30:02 +0100

    Update ControllerEventInterface.php

--- src/Interfaces/ControllerEventInterface.php
+++ src/Interfaces/ControllerEventInterface.php
@@ -2,9 +2,9 @@
 namespace Devoir\Interfaces;
 
 /**
- * ControllerEventInterface
+ * ControllerEventInterface, Interfaces for controller's events.
  * @namespace Devoir\Interfaces
- * @author Muhammad Tahir Abdullahi
+ * @author Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
  * @copyright Copyright (c) Elftech Inc.
  * @package elfwap/devoir
  * @license https://opensource.org/licenses/mit-license.php MIT License
@@ -13,34 +13,33 @@ namespace Devoir\Interfaces;
 interface ControllerEventInterface extends DevoirEventInterface
 {
 	/**
-	 * 
+	 * Invoked before the application runs, before calling the `run` function.
 	 * @param DevoirEventInterface $event
 	 */
 	public function beforeRunUp(ControllerEventInterface $event);
 	/**
-	 * 
+	 * Invoked after the application runs, after calling the run function successfully.
 	 * @param DevoirEventInterface $event
 	 */
 	public function afterRunUp(ControllerEventInterface $event);
 	/**
-	 * 
+	 * Invoked before data are being dispatched to the `Models`. 
 	 * @param DevoirEventInterface $event
 	 */
 	public function beforeDispatch(ControllerEventInterface $event);
 	/**
-	 * 
+	 * Invoked after data are being dispatched to the `Models` successfully.
 	 * @param DevoirEventInterface $event
 	 */
 	public function afterDispatch(ControllerEventInterface $event);
 	/**
-	 * 
+	 * Invoked before the manifestation of the current `View`.
 	 * @param ControllerEventInterface $event
 	 */
 	public function beforeManifest(ControllerEventInterface $event);
 	/**
-	 * 
+	 * Invoked after the manifest of the current `View`.
 	 * @param ControllerEventInterface $event
 	 */
 	public function afterManifest(ControllerEventInterface $event);
 }
-
