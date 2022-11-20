223721ce9b2058b96efde29d9f859ba503a6ec16
Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
2022-11-20 22:41:45 +0100

    #++src

---
+++ src/Interfaces/ModelEventInterface.php
@@ -0,0 +1,16 @@
+<?php
+namespace Devoir\Interfaces;
+
+/**
+ * Model Event Interface, should be implemented by the Models.
+ * @namespace Devoir\Interfaces
+ * @author Muhammad Tahir Abdullahi <muhammedtahirabdullahi@gmail.com>
+ * @copyright Copyright (c) Elftech Inc.
+ * @package elfwap/devoir
+ * @license https://opensource.org/licenses/mit-license.php MIT License
+ *        
+ */
+interface ModelEventInterface
+{
+}
+
